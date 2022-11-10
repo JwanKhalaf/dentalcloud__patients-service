34fe4bbe750ac0e81a0b4f0bb06697f6d1b7fd95
Jwan Khalaf <jwan.khalaf@outlook.com>
2022-11-10 22:18:17 +0000

    add test to validate first name

--- api/patients/create/handler.go
+++ api/patients/create/handler.go
@@ -1,9 +1,44 @@
 package create
 
-import "net/http"
+import (
+	"encoding/json"
+	"mime"
+	"net/http"
+)
+
+type CreatePatientRequest struct {
+	FirstName string `json:"first_name"`
+}
 
 func CreatePatientHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
+		// enforce a json content-type
+		contentType := r.Header.Get("content-type")
+
+		mediatype, _, err := mime.ParseMediaType(contentType)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if mediatype != "application/json" {
+			http.Error(w, "api expects application/json content-type", http.StatusUnsupportedMediaType)
+			return
+		}
+
+		dec := json.NewDecoder(r.Body)
+		dec.DisallowUnknownFields()
+		var createPatientRequest CreatePatientRequest
+		if err := dec.Decode(&createPatientRequest); err != nil {
+			http.Error(w, "request body is invalid", http.StatusBadRequest)
+			return
+		}
+
+		if createPatientRequest.FirstName == "" {
+			http.Error(w, "request body is invalid", http.StatusBadRequest)
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
 	})
 }
--- api/patients/create/handler_test.go
+++ api/patients/create/handler_test.go
@@ -1,15 +1,44 @@
 package create
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestCreatePatient(t *testing.T) {
-	t.Run("create returns 400 when missing required attributes", func(t *testing.T) {
+	t.Run("returns 415 (unsupported media type) when the request does not have content-type set as application/json", func(t *testing.T) {
 		// create a request to pass to our handler
 		req, _ := http.NewRequest("POST", "/patients", nil)
+
+		// set the content type
+		req.Header.Set("content-type", "text/csv")
+
+		// create a response recorder
+		res := httptest.NewRecorder()
+
+		// get the handler
+		handler := CreatePatientHandler()
+
+		// our handler satisfies http.handler, so we can call its serve http method
+		// directly and pass in our request and response recorder
+		handler.ServeHTTP(res, req)
+
+		// assert status code is what we expect
+		assertStatusCode(t, res.Code, http.StatusUnsupportedMediaType)
+	})
+
+	t.Run("create returns 400 (bad request) when first name is not set in the body", func(t *testing.T) {
+
+		jsonValue, _ := json.Marshal(CreatePatientRequest{})
+
+		// create a request to pass to our handler
+		req, _ := http.NewRequest("POST", "/patients", bytes.NewBuffer(jsonValue))
+
+		// set the content type
+		req.Header.Set("content-type", "application/json")
 
 		// create a response recorder
 		res := httptest.NewRecorder()
