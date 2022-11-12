423cb1a875255826930a0d708f15ba035310bf5a
Jwan Khalaf <jwan.khalaf@outlook.com>
2022-11-12 23:41:32 +0000

    add code to put new patient into dynamodb table

--- api/patients/create/lambda/main.go
+++ api/patients/create/lambda/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/akrylysov/algnhsa"
+	"github.com/jwankhalaf/dentalcloud__patients-service/api/patients"
+	"github.com/jwankhalaf/dentalcloud__patients-service/api/patients/create"
+	"go.uber.org/zap"
+)
+
+func main() {
+	// initialise a new zap logger
+	logger, _ := zap.NewProduction()
+
+	logger.Info("running the create patient lamdba...")
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/", create.CreatePatientHandler(logger, patients.NewPatientStore(logger)))
+	algnhsa.ListenAndServe(mux, nil)
+}
